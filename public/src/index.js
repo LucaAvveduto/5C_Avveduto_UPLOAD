e2e3cd19661138f93690b6924607dcdd79c835b2
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2025-02-09 17:31:24 +0100

    link list

--- public/src/index.js
+++ public/src/index.js
@@ -1,10 +1,28 @@
+const renderList = async() => {
+    const link = document.querySelector("#link");
+    const files = await fetch("/list");
+    const images = await files.json();
+    const urls = Array.from(images);
+    const template = `<li><a href='%URL' target="_blank">%URL</a></li>`;
+    let html = "<ul>"
+    html += urls.map((e) => {
+      return template.replaceAll("%URL",e);
+    }).join("");
+    html += "</ul>";
+    link.innerHTML = html;
+}
+
 (async () => {
     const inputFile = document.querySelector('#file');
     const button = document.querySelector("#button");
-    const link = document.querySelector("#link");
+    const errorDiv = document.querySelector("#error");
   
     handleSubmit = async (event) => {
       const formData = new FormData();
+      if (!inputFile.files[0]) {
+        errorDiv.innerHTML = "<p>Insert a file</p>";
+        return;
+      }
       formData.append("file", inputFile.files[0]);
       const body = formData;
       const fetchOptions = {
@@ -14,12 +32,16 @@
       try {
         const res = await fetch("/upload", fetchOptions);
         const data = await res.json();
-        link.setAttribute("href", data.url);
-        link.innerText = data.url;
+        await renderList();
       } catch (e) {
         console.log(e);
       }
     }
-  
     button.onclick = handleSubmit;
   })();
+
+  renderList().then(() => {
+    setInterval(async() => {
+        await renderList();
+    },30000)
+  }).catch(console.error)
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require('http');
 const path = require('path');
-const app = express();
 const multer  = require('multer');
+const fs = require("fs");
+
+const app = express();
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, path.join(__dirname, "files"));
@@ -17,11 +19,17 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/files", express.static(path.join(__dirname, "files")));
 app.post('/upload', (req, res) => {
     upload(req, res, (err) => {
-        
-    console.log(req.file.filename);    
     res.json({url: "./files/" + req.file.filename});    
   })
 });
+
+app.get("/list",(req,res) => {
+    const files = fs.readdirSync(path.join(__dirname, 'files'));
+    let filesPath = files.map((file) => {
+    return ("./files/" + file);
+  });
+  res.json(filesPath);    
+})
 
 const server = http.createServer(app);
 server.listen(5600, () => {
